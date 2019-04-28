ac69a01ea6fad5d26625a2b75c83f81ef25f64bd
Will Wetzel <william@wetzel.co.uk>
2019-04-28 02:47:41 +0100

    gradle update and started calculations for Cmin

--- app/src/main/java/com/example/alarmcalculations/MainActivity.java
+++ app/src/main/java/com/example/alarmcalculations/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.alarmcalculations;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,41 @@ public class MainActivity extends AppCompatActivity {
     /** Called when calculate button is pressed **/
     public void calculateBatteryCapacity(View view)
     {
+        //Define Variables & Fetch from inputs
+        TextView resultView = (TextView)findViewById(R.id.textResult);
+        double standbyCurrent = Double.parseDouble(findViewById(R.id.editStandByCurrent));
+        double fullAlarmCurrent = Double.parseDouble(editAlarmCurrent);
+        double standByTime = Double.parseDouble(editStandbyTime);
+        double Cmin = 0;
+
+
+
+
+
+        double value;
+        String text =your_edittext.getText().toString();
+        if(!text.isEmpty())
+            try
+            {
+                value= Double.parseDouble(text);
+                // it means it is double
+            } catch (Exception e1) {
+                // this means it is not double
+                e1.printStackTrace();
+            }
+
+
+
+
+
+        //Cmin Calculations
+        Cmin = standbyCurrent * standByTime;
+        Cmin += fullAlarmCurrent;
+        Cmin = Cmin * 1.25;
+
+        //Send to TextView
+        String finalResult = Double.toString(Cmin);
+        resultView.setText(finalResult);
 
     }
 
